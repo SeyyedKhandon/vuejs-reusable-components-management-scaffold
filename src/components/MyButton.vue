595dcd0cb68db1a9b0d617800f77919749972828
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-06-16 10:10:17 +0430

    add storybook via 'vue add storybook', add license

---
+++ src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button is-primary" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "my-button"
+});
+</script>
+
+<style scoped>
+button {
+  border: 1px solid #eee;
+  border-radius: 3px;
+  background-color: #ffffff;
+  cursor: pointer;
+  font-size: 15pt;
+  padding: 3px 10px;
+  margin: 10px;
+}
+</style>
