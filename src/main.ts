29bde48786a21691b3755e1e3192940de13865ae
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-06-16 09:32:10 +0430

    add prettier, use npm instead of yarn(npm i), add vue-composition-api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
+Vue.use(VueCompositionApi);
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
